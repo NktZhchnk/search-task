19c2003eb5f1a3c42e417ded5a0e7344e7664a58
niktia <mr.nikita.zhuchenko@gmail.com>
2022-02-17 23:14:51 +0200

    nedo UPDATE v0.000001

--- src/index.js
+++ src/index.js
@@ -1,69 +1,6 @@
-// const temp = document.querySelector('.temp')
-// const ul = document.querySelector('.ul')
-// const div = document.getElementById('div')
-//
-// import './weatherData'
-// // const tempRounding = (data)=>{
-// //     let tempFloor = temp.textContent = data.main.temp;
-// //     Math.round(tempFloor)
-//
-// const getWeather = () => {
-//     return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=47.49&lon=35.11&units=metric&exclude=minutely&appid=055482ee48aad4c8958c811cd83af106')
-//         //https://api.openweathermap.org/data/2.5/weather?id=687676&units=metric&appid=055482ee48aad4c8958c811cd83af106
-//         .then(function (resp) {
-//             return resp.json()
-//         })
-//         .catch(function () {
-//             console.log('FUCK ERROR')
-//         })
-//
-// };
-//
-// const proceedWeather = async () => {
-//     const data = await getWeather()
-//     let tempFloor = data.daily[0].temp.day; // 1.345
-//     let tempRounding = Math.round(tempFloor); // 1
-//     let dayUp = data.daily.flat()  // {} - 8 {} {} {} {}\
-//     let mas = [];
-//     const dayTemp = () => {
-//         for (let day of dayUp) {
-//            day = day.temp.day
-//             mas.push(day)
-//             console.log(mas)
-//         }
-//     }
-// dayTemp()
-//     const createCard = (card) => {
-//         const cardElement = document.createElement('div');
-//         cardElement.className = 'card';
-//         cardElement.innerHTML = `
-//     <div>hello</div>`
-//         return cardElement;
-//     }
-//
-//
-//     mas.forEach((card) => {
-//         const cardElem = createCard(card)
-//         div.appendChild(cardElem)
-//         console.log(card)
-//     })
-//
-//    // console.log(dayUp)
-//     //console.log(day.temp.day)
-//     // console.log(dayUp);
-//     // console.log(data);
-//
-//     // document.querySelector('.nameCity').textContent = data.name;
-//     // temp.textContent = tempRounding + '°';
-// }
-//
-// proceedWeather()
-//
 const input = document.querySelector('.input')
-const btn = document.querySelector('.btn')
 const textField = document.querySelector('.text')
-let searchText = '';
-
+const counterSearch = document.querySelector('.counterSearch')
 textField.innerHTML = `
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
 The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
@@ -72,8 +9,7 @@ Many desktop publishing packages and web page editors now use Lorem Ipsum as the
 and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
 Various versions have evolved over the years, sometimes by accident, sometimes on purpose
   `
-
-let text2 = textField.innerText;
+counterSearch.innerHTML = ''
 const searchTextFull = (text, search) => {
     let mas = [];
     let lastPosition = 0;
@@ -83,8 +19,11 @@ const searchTextFull = (text, search) => {
         lastPosition = textPosition + search.length
         mas.push(textPosition)
     }
+    counterSearch.innerHTML = mas.length
     return mas;
 };
+
+
 
 input.addEventListener('input', () => {
     const searchText = input.value
