fa7ac681eeb144bd0e94846bdf613adb79d2fdec
niktia <mr.nikita.zhuchenko@gmail.com>
2022-07-15 23:15:10 +0300

    rollback Position input-search

--- src/index.js
+++ src/index.js
@@ -1,74 +1,131 @@
-const searchInitial = (input, textField, counterSearch) => {
-    textField.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-    counterSearch.innerHTML = 0
-    const searchTextFull = (text, search) => {
-        let mas = [];
-        let lastPosition = 0;
-        while (true) {
-            const textPosition = text.indexOf(search, lastPosition)
-            if (textPosition === -1) break;
-            lastPosition = textPosition + search.length
-            mas.push(textPosition)
-        }
-        counterSearch.innerHTML = mas.length
-        return mas;
-    };
+const nameValue = document.querySelector('.nameValue');
+const phoneValue = document.querySelector('.phoneValue');
+const priceValue = document.querySelector('.priceValue');
+const btnInTable = document.querySelector('.btnInTable');
+const searchInput = document.querySelector('.inputSearch')
+const openSearch = document.querySelector('.open-search')
+const divSearch = document.querySelector('.div-search')
+btnInTable.addEventListener("click", () => {
+    addTable();
+});
+let toggle = false;
 
-    input.addEventListener('input', ()=>{
-        const searchText = input.value
-        const text = textField.innerText
-        if (searchText.length === 0) {
-            counterSearch.innerHTML = 0
-            textField.innerHTML = text
-            return
-        }
-        const mas = searchTextFull(text, searchText)
-        console.log(mas)
-        let position = 0;
-        textField.innerHTML = '';
-        mas.forEach((searchPosition) => {
-            textField.innerHTML += text.slice(position, searchPosition)
-                + '<span class="searched-fragment">' + text.slice(searchPosition, searchPosition + searchText.length) + '</span>'
-            position = searchPosition + searchText.length;
+let tableData = [];
+let tableDataFiltered = [];
+let autoId = 1;
+
+
+const createRow = (data) => {
+    const {phone, price, name, id} = data;
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${phone}</td>
+        <td>${price}</td>
+        <td class="fixBorder">
+            <button class="button-remove">x</button>
+        </td>
+    `;
+    const remove = row.querySelector('button');
+
+    remove.addEventListener('click', () => {
+        row.remove();
+        tableData = tableData.filter((item) => item.id !== id);
+    });
+    return row;
+}
+
+
+
+const addTable = () => {
+    const name = nameValue.value;
+    const phone = phoneValue.value;
+    const price = priceValue.value;
+
+    nameValue.value = '';
+    phoneValue.value = '';
+    priceValue.value = '';
+
+    createTableItem({name, phone, price});
+};
+
+const createTableItem = (data) => {
+    const id = autoId++;
+    tableData.push({...data, id});
+
+    searchFn();
+    render();
+}
+
+const lookInput = () => {
+    searchFn()
+    render()
+}
+
+let searchTimer = null
+
+searchInput.addEventListener('input', () => {
+    clearTimeout(searchTimer)
+    searchTimer = setTimeout(lookInput, 500);
+})
+
+
+const searchFn = () => {
+    const comparisonFn = (a, b) => b.toLowerCase().indexOf(a.toLowerCase()) !== -1;
+    let data = searchInput.value;
+
+    if (data.trim().length === 0) {
+        tableDataFiltered = tableData;
+        return;
+    }
+
+    tableDataFiltered = [];
+    tableData.forEach((item) => {
+        let check = false;
+        ['name', 'phone'].forEach((key) => {
+            if (comparisonFn(data, item[key])) {
+                check = true
+            }
         })
-        textField.innerHTML += text.slice(position)
-    })
+        if (check) {
+            tableDataFiltered.push(item)
+        }
+    });
+};
+
+const render = () => {
+    const table = document.querySelector('.table tbody');
+    table.innerHTML = '';
+
+
+    tableDataFiltered.forEach((item) => {
+        table.appendChild(createRow(item));
+    });
 }
 
+openSearch.addEventListener("click", () => {
+    if (toggle === false) {
+        searchInput.style.display = "block"
+        toggle = true
+        const backSearch = document.createElement("button");
+        backSearch.innerHTML = `x`;
+        backSearch.style.width = "25px";
+        backSearch.style.height = "25px";
+        backSearch.style.position = "relative";
+        backSearch.style.left = "455px";
+        backSearch.addEventListener('click', () => {
+            openSearch.style.display = "block";
+            searchInput.style.display = "none";
+            backSearch.remove()
+            toggle = false;
+            searchInput.value = '';
+            lookInput();
+        });
+        divSearch.appendChild(backSearch);
+        openSearch.style.display = "none";
+    } else {
+        searchInput.style.display = "none";
 
-searchInitial(document.querySelector('.search-input'), document.querySelector('.text'), document.querySelector('.search-counter'))
-
-const droppedMenuRedact = (redact, textField, buttonsMenu, cancel, accept) => {
-    const footer = document.querySelector('.footer')
-    const input = document.querySelector('.textarea-redact')
-    const styleRedact = () => {
-        buttonsMenu.style.display = "block";
-        redact.style.display = "none";
-        footer.style.border = "none";
-        textField.style.display = "none";
-        input.style.display = "block"
     }
-    let tempValue = input.value = textField.textContent
-    redact.addEventListener('click', () => {
-        styleRedact()
-    })
-
-    const hideButtons = () => {
-        buttonsMenu.style.display = "none";
-        redact.style.display = "block";
-        textField.style.border = "none";
-        textField.style.display = "block";
-        input.style.display = "none";
-        footer.style.border = "1px solid rgba(0, 0, 0, 0.4)";
-    }
-    cancel.addEventListener('click', () => {
-        hideButtons()
-        textField.innerText = textField.textContent
-        input.value = textField.textContent
-    })
-    accept.addEventListener('click', () => {
-        hideButtons()
-        textField.innerText = document.querySelector('.textarea-redact').value
-    })
-}
-droppedMenuRedact(document.querySelector('.btn-dropped-menu'), document.querySelector('.text'), document.querySelector('.buttons-menu'), document.querySelector('.btn-cancel'), document.querySelector('.btn-accept'));
+});
